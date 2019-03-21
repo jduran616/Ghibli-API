3244d76d5b887651e81216f32f674122812c9f42
JD <jduran005@student.butte.edu>
2019-03-21 09:04:19 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// create request variable
+var req = new XMLHttpRequest();
+
+// Open connection using GET on URL endpoint
+req.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+requestAnimationFrame.onload = function() {
+    
+}
